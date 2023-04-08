c9034acda1a8dae0b4054f210dfb3b8a93c12826
austinjstromme <austinjstromme@gmail.com>
2023-04-08 16:37:49 -0400

    Got basic world map plot working, need to figure out how to access local json files

---
+++ coffee-app/src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import * as d3 from "d3";
+    import GlobalMap from "./GlobalMap.svelte";
+    let dataset = [];
+    d3.json(
+       "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+    ).then((data) => {
+      dataset = data.features;
+    });
+    const width = 1200,
+      height = 600;
+  </script>
+  
+  <main>
+    <svg {width} {height}>
+      <GlobalMap {dataset} />
+    </svg>
+  </main>
---
+++ coffee-app/src/components/GlobalMap.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { geoPath, geoNaturalEarth1 } from "d3";
+    export let dataset = [];
+    import { draw } from "svelte/transition";
+    import { quadInOut } from "svelte/easing";
+    const projection = geoNaturalEarth1();
+    const path = geoPath(projection);
+  </script>
+  
+  {#each dataset as data}
+    <path
+      d={path(data)}
+    />
+  {/each}
+  
+  <style>
+    path {
+      fill: none;
+      stroke: darkgreen;
+    }
+  </style>
