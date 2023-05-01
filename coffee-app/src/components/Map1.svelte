1927efee85ffb65c974bd236e859bba17db62290
austinjstromme <austinjstromme@gmail.com>
2023-05-01 14:16:51 -0400

    Got Honduras drought data appearing

--- coffee-app/src/components/Map1.svelte
+++ coffee-app/src/components/Map1.svelte
@@ -1,5 +1,6 @@
 <script>
     import mapboxgl from "mapbox-gl";
+    import * as d3 from "d3";
     import { onMount } from "svelte";
     export let index;
     export let geoJsonToFit;
@@ -9,6 +10,8 @@
   
     let container;
     let map;
+
+    let hondurasDroughtData;
   
     let zoomLevel;
 
@@ -17,6 +20,12 @@
       {"type":"Feature","properties":{"name":"Guatemala", "coffee":3606},"geometry":{"type":"Polygon","coordinates":[[[-90.095555,13.735338],[-90.608624,13.909771],[-91.23241,13.927832],[-91.689747,14.126218],[-92.22775,14.538829],[-92.20323,14.830103],[-92.087216,15.064585],[-92.229249,15.251447],[-91.74796,16.066565],[-90.464473,16.069562],[-90.438867,16.41011],[-90.600847,16.470778],[-90.711822,16.687483],[-91.08167,16.918477],[-91.453921,17.252177],[-91.002269,17.254658],[-91.00152,17.817595],[-90.067934,17.819326],[-89.14308,17.808319],[-89.150806,17.015577],[-89.229122,15.886938],[-88.930613,15.887273],[-88.604586,15.70638],[-88.518364,15.855389],[-88.225023,15.727722],[-88.68068,15.346247],[-89.154811,15.066419],[-89.22522,14.874286],[-89.145535,14.678019],[-89.353326,14.424133],[-89.587343,14.362586],[-89.534219,14.244816],[-89.721934,14.134228],[-90.064678,13.88197],[-90.095555,13.735338]]]},"id":"GTM"},
       {"type":"Feature","properties":{"name":"El Salvador", "coffee":661},"geometry":{"type":"Polygon","coordinates":[[[-87.793111,13.38448],[-87.904112,13.149017],[-88.483302,13.163951],[-88.843228,13.259734],[-89.256743,13.458533],[-89.812394,13.520622],[-90.095555,13.735338],[-90.064678,13.88197],[-89.721934,14.134228],[-89.534219,14.244816],[-89.587343,14.362586],[-89.353326,14.424133],[-89.058512,14.340029],[-88.843073,14.140507],[-88.541231,13.980155],[-88.503998,13.845486],[-88.065343,13.964626],[-87.859515,13.893312],[-87.723503,13.78505],[-87.793111,13.38448]]]},"id":"SLV"},
     ]}
+
+    d3.json(
+      "https://raw.githubusercontent.com/6C85-CoffeeTeam/CoffeeTeam/main/coffee-app/src/data/honduras_drought_risk.geojson"
+    ).then((data) => {
+      hondurasDroughtData = data;
+    });
   
     function updateZoomLevel() {
       const screenWidth = window.innerWidth;
@@ -67,6 +76,11 @@
             type: 'geojson',
             data: norTriData
         });
+
+      map.addSource('hondurasDrought', {
+            type: 'geojson',
+            data: hondurasDroughtData
+        });
   
         map.addLayer({
             id: 'country-fills',
@@ -89,6 +103,42 @@
             'paint': {
             'line-color': '#627BC1',
             'line-width': 0
+            }
+          });
+
+        map.addLayer({
+            'id': 'HondurasDroughtLow',
+            filter: ['==', ['get', 'Dr_Text'], 'Low'],
+            'type': 'fill',
+            'source': 'hondurasDrought',
+            'layout': {},
+            'paint': {
+              'fill-color': '#A59555',
+              'fill-opacity': 0.4
+            }
+          });
+
+          map.addLayer({
+            'id': 'HondurasDroughtMedium',
+            filter: ['==', ['get', 'Dr_Text'], 'Medium'],
+            'type': 'fill',
+            'source': 'hondurasDrought',
+            'layout': {},
+            'paint': {
+              'fill-color': '#DD9d12',
+              'fill-opacity': 0.4
+            }
+          });
+
+          map.addLayer({
+            'id': 'HondurasDroughtHigh',
+            filter: ['==', ['get', 'Dr_Text'], 'High'],
+            'type': 'fill',
+            'source': 'hondurasDrought',
+            'layout': {},
+            'paint': {
+              'fill-color': '#E12D02',
+              'fill-opacity': 0.4
             }
           });
       });
