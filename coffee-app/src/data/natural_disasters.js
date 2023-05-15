32856886563f48ab6426f0d96a29c275d4d031eb
Eunhae Lee <eunhae1570@gmail.com>
2023-05-15 01:34:30 -0400

    added natural disasters data

---
+++ coffee-app/src/data/natural_disasters.js
@@ -0,0 +1,182 @@
+export let naturalDisasters =[
+    {
+      "Country": "Honduras",
+      "Year": 2020,
+      "Event": "Storm",
+      "Impact": 5145784
+    },
+    {
+      "Country": "Honduras",
+      "Year": 1998,
+      "Event": "Storm",
+      "Impact": 2112000
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2014,
+      "Event": "Drought",
+      "Impact": 571710
+    },
+    {
+      "Country": "Honduras",
+      "Year": 1999,
+      "Event": "Flood",
+      "Impact": 503001
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2016,
+      "Event": "Flood",
+      "Impact": 450500
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2018,
+      "Event": "Drought",
+      "Impact": 360000
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2008,
+      "Event": "Flood",
+      "Impact": 313357
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2001,
+      "Event": "Drought",
+      "Impact": 195000
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2004,
+      "Event": "Drought",
+      "Impact": 137500
+    },
+    {
+      "Country": "Honduras",
+      "Year": 2012,
+      "Event": "Drought",
+      "Impact": 125000
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2020,
+      "Event": "Storm",
+      "Impact": 2854072
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2009,
+      "Event": "Drought",
+      "Impact": 2500000
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2018,
+      "Event": "Drought",
+      "Impact": 1500000
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2014,
+      "Event": "Drought",
+      "Impact": 1300000
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2011,
+      "Event": "Flood",
+      "Impact": 534828
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2005,
+      "Event": "Storm",
+      "Impact": 475314
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2010,
+      "Event": "Storm",
+      "Impact": 400142
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2012,
+      "Event": "Drought",
+      "Impact": 266485
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2008,
+      "Event": "Flood",
+      "Impact": 197300
+    },
+    {
+      "Country": "Guatemala",
+      "Year": 2001,
+      "Event": "Drought",
+      "Impact": 113596
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2014,
+      "Event": "Drought",
+      "Impact": 700000
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2001,
+      "Event": "Drought",
+      "Impact": 400000
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2018,
+      "Event": "Drought",
+      "Impact": 386610
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2011,
+      "Event": "Flood",
+      "Impact": 300000
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2020,
+      "Event": "Storm",
+      "Impact": 149840
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2009,
+      "Event": "Storm",
+      "Impact": 90000
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 1998,
+      "Event": "Storm",
+      "Impact": 84000
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 2005,
+      "Event": "Storm",
+      "Impact": 72141
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 1982,
+      "Event": "Flood",
+      "Impact": 68000
+    },
+    {
+      "Country": "El Salvador",
+      "Year": 1988,
+      "Event": "Flood",
+      "Impact": 39060
+    }
+   ]
